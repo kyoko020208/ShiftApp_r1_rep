a1ec747d5b7bf0e04f111903e6be7d015301e2e8
kyoko020208 <kyoko020208@gmail.com>
2018-11-21 21:17:52 -0800

    Initial Version. - created models.py - added the following auth info to setting.py LOGIN_ERROR_URL = '/accounts/login' LOGIN_URL = '/accounts/login/' LOGIN_REDIRECT_URL = '/accounts/index/' AUTH_USER_MODEL = 'accounts.AuthUser'
    -created index.html
    -created login.html

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -5,7 +5,7 @@ from accounts.views import Index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', LoginView.as_view(template_name='account/login.html'), name='login'),
+    url(r'^accounts/login/$', LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^accounts/logout/$', logout_then_login, name='logout'),
     url(r'^accounts/index/$', Index.as_view(), name='account_index'),
 ]
